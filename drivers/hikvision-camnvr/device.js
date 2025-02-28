77d76930a2598d46e938def8c8689f12c5c012cb
limkopi78 <32301049+limkopi78@users.noreply.github.com>
2025-02-28 16:35:27 +0800

    Update device.js to SDK3

--- drivers/hikvision-camnvr/device.js
+++ drivers/hikvision-camnvr/device.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const Homey = require('homey');
-const axios = require('axios');
-const xml2js = require('xml2js');
 const HikvisionAPI = require('./hikvision.js').hikvisionApi;
 
 class HikCamera extends Homey.Device {
@@ -29,63 +27,23 @@ class HikCamera extends Homey.Device {
     async updateCapabilities() {
         this.log('Updating Capabilities');
         try {
-            const protocol = this.settings.ssl ? 'https://' : 'http://';
-            const url = `${protocol}${this.settings.address}:${this.settings.port}/ISAPI/System/deviceInfo`;
-            
-            const response = await axios.get(url, {
-                auth: {
-                    username: this.settings.username,
-                    password: this.settings.password
-                },
-                httpsAgent: new (require('https').Agent)({ rejectUnauthorized: this.settings.strict }),
-            });
-
-            const result = await xml2js.parseStringPromise(response.data);
-            const deviceType = result?.DeviceInfo?.deviceType?.[0] || 'Unknown';
-            const firmwareVersion = result?.DeviceInfo?.firmwareVersion?.[0] || 'Unknown';
-
-            await this.setCapabilityValue("hik_type", deviceType);
-            await this.setCapabilityValue("hik_version", firmwareVersion);
-            this.log(`Device Type: ${deviceType}, Firmware Version: ${firmwareVersion}`);
-        } catch (error) {
-            this.error('Failed to update capabilities:', error.message);
-        }
-    }
-
-    async onSettings(oldSettings, newSettings, changedKeys) {
-        this.settings = newSettings;
-        await this.updateCapabilities();
-        this.connectToHikvision();
-    }
-
-    async connectToHikvision() {
-        try {
-            this.log('Connecting to Hikvision device...');
-            
-            const options = {
+            this.hikApi = new HikvisionAPI({
                 host: this.settings.address,
                 port: this.settings.port,
                 ssl: this.settings.ssl,
                 strict: this.settings.strict,
                 user: this.settings.username,
                 pass: this.settings.password,
-                log: false,
-            };
-            
-            this.hikApi = new HikvisionAPI(options);
+                log: true
+            });
 
             this.hikApi.on('socket', async () => {
                 await this.handleConnection('connect');
                 await this.driver._triggers.trgOnConnected.trigger(this);
             });
 
-            this.hikApi.on('close', async () => {
-                await this.handleConnection('disconnect');
-                await this.driver._triggers.trgOnDisconnected.trigger(this);
-            });
-
             this.hikApi.on('error', async (error) => {
-                this.log('Hikvision API Error:', error);
+                this.error('Hikvision API Error:', error);
                 await this.handleConnection('error');
                 await this.driver._triggers.trgOnError.trigger(this);
             });
@@ -109,8 +67,9 @@ class HikCamera extends Homey.Device {
                     }
                 }
             });
+
         } catch (error) {
-            this.error('Failed to connect to Hikvision:', error.message);
+            this.error('Failed to update capabilities:', error.message);
         }
     }
 
@@ -121,34 +80,6 @@ class HikCamera extends Homey.Device {
         } else if (status === 'connect') {
             this.setAvailable();
             await this.setCapabilityValue("hik_status", true);
-        }
-    }
-
-    async ptzZoom(pan, tilt, zoom, channel) {
-        try {
-            const isNVR = this.getCapabilityValue('hik_type') === "NVR";
-            const PTZurl = isNVR
-                ? `:${this.settings.port}/ISAPI/ContentMgmt/PTZCtrlProxy/channels/${channel}/continuous`
-                : `:${this.settings.port}/ISAPI/PTZCtrl/channels/${channel}/continuous`;
-
-            const protocol = this.settings.ssl ? 'https://' : 'http://';
-            const url = `${protocol}${this.settings.address}${PTZurl}`;
-            
-            const body = `<PTZData><pan>${pan}</pan><tilt>${tilt}</tilt><zoom>${zoom}</zoom></PTZData>`;
-            
-            await axios.put(url, body, {
-                auth: {
-                    username: this.settings.username,
-                    password: this.settings.password
-                },
-                headers: { 'Content-Type': 'application/xml' },
-                httpsAgent: new (require('https').Agent)({ rejectUnauthorized: this.settings.strict })
-            });
-
-            return true;
-        } catch (error) {
-            this.error('PTZ Zoom Command Failed:', error.message);
-            return false;
         }
     }
 }
